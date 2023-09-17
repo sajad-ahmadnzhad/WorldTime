0801a6a8a6fbb4fa41e8d3e631bb3e1f0c5598be
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2023-09-17 20:59:33 +0430

    API bug fixes

--- bot.ts
+++ bot.ts
@@ -1,14 +1,8 @@
 let telegramBot = require("node-telegram-bot-api");
-
-(async()=> {
-let data = await (await fetch('http://worldtimeapi.org/api/timezone/America')).json()
-// console.log(data);
-
-})()
-
 let token = "6411892572:AAH5dGhr3Ej8feFkhiuzPsR4GBObcD0nspM";
 
-let bot = new telegramBot(token, { polling: true });
+let bot = new telegramBot( token, {polling: true });
+
 
 bot.onText(/\/start/, async (msg: typeof bot) => {
   let id = msg.from.id;
@@ -24,6 +18,7 @@ For example: "Asia Tehran or Africa Algiers or America Adak or America Indiana M
     
 Otherwise, the robot will not return a message to you
     `
+
     await bot.sendChatAction(id, "typing");
     bot.sendMessage(id, message);
 });
@@ -31,7 +26,6 @@ Otherwise, the robot will not return a message to you
 bot.on("message", (msg: typeof bot) => {
   let id = msg.from.id;
   if (msg.text == "/start") return;
-console.log(msg);
 
     let message = new MessageText(id , msg.text)
     message.desplayMessage()
@@ -39,20 +33,19 @@ console.log(msg);
 });
 
 interface ResponseData {
-    abbreviation: string
-    client_ip: string
-    datetime: string
-    day_of_week: number
-    day_of_year: number
-    raw_offset: number
-    timezone: string
-    unixtime: number
-    utc_datetime: string
-    utc_offset: string
-    week_number: string
-    error: string
+    abbreviation?: string
+    client_ip?: string
+    datetime?: string
+    day_of_week?: number
+    day_of_year?: number
+    raw_offset?: number
+    timezone?: string
+    unixtime?: number
+    utc_datetime?: string
+    utc_offset?: string
+    week_number?: string
+    error?: string
 }
-
 
 class MessageText {
   constructor(protected senderId: number, protected content: string) {}
@@ -61,11 +54,16 @@ class MessageText {
       let splitData = this.content.split(' ')
       let sliceData = splitData.slice(1)
      await bot.sendChatAction(this.senderId , 'typing')
-      let response: ResponseData = await (await fetch(`http://worldtimeapi.org/api/timezone/${splitData[0]}/${sliceData}`)).json()
-      
-    if (response.error)return bot.sendMessage(this.senderId , response.error)
+      let response: ResponseData = {}
+     try {
+      response = await (await fetch(`http://worldtimeapi.org/api/timezone/${splitData[0]}/${sliceData}`)).json()
+     } catch (error) {
+      bot.sendMessage(this.senderId , 'The server encountered a problem, please try again')
+     }
+     
+     if (response.error)return bot.sendMessage(this.senderId , response.error)
 
-    let result = `data: \n
+    let result = `data: \n 
 abbreviation: ${response.abbreviation}\n
 your ip: ${response.client_ip}\n
 datetime: ${response.datetime}\n
